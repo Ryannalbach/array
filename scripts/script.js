2bc3a5b3c2c14cd0aa2cad09bd6d2fba5896de80
Ryan Nalbach <103783337+Ryannalbach@users.noreply.github.com>
2022-07-16 14:10:39 -0700

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,11 @@ let numbers = [
     [1,2,3,4]
     [5,6,7,8]
     [9,10,11,12]
+];
+
+
+var letters = [
+ ['a', 'b', 'c', 'd']
+ ['e', 'f', 'g', 'h']
+ ['i', 'j', 'k', 'l']
 ]
-console.log(numbers)
-return numbers;
